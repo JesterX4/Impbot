c076aebbc14435467bf3f49ed6da9d8bebc29e7a
JesterX4 <ortaligdanmaga@gmail.com>
2026-02-16 18:01:33 +0300

    bot yeni surum

--- index.js
+++ index.js
@@ -253,3 +253,14 @@ console.log("Unhandled Promise Rejection:", err);
 process.on("uncaughtException", err => {
 console.log("Uncaught Exception:", err);
 });
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+res.send("Bot is alive!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+console.log("Web server running...");
+});
